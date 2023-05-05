03d56c1a08347c7ad356b7e099444a5c89036644
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-05 11:20:30 +0300

    Animation Library

---
+++ mo.js
@@ -0,0 +1,13 @@
+const bouncyCircle = new mojs.Shape({
+  parent:       '#bouncyCircle',
+  shape:        'circle',
+  fill:         {'#F64040': '#FC46AD'},
+  radius:       {20: 80},
+  duration:     2000,
+  isYoyo:       true,
+  isShowStart:  true,
+  easing:       'elastic.inout',
+  repeat:       1,
+});
+
+bouncyCircle.play()
