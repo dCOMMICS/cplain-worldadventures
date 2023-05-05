110b1fab5ee81a9c17333fb1e98a62c80f9b4ee2
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-05 11:36:41 +0300

    Fixing :code*2

--- mo.js
+++ mo.js
@@ -11,3 +11,30 @@ const bouncyCircle = new mojs.Shape({
 });
 
 bouncyCircle.play()
+
+
+var circle = new mojs.Shape({
+    shape:        'circle',
+    radius:       10,
+    radiusX:      20, // explicit radiusX
+    left:         '25%',
+    fill:         'deeppink',
+    isShowStart:  true,
+  });
+  
+  var rect = new mojs.Shape({
+    shape:        'rect',
+    radius:       15,
+    left:         '50%',
+    fill:         'cyan',
+    isShowStart:  true,
+  });
+  
+  var polygon = new mojs.Shape({
+    shape:        'polygon',
+    radiusX:      10,
+    radiusY:      20,
+    left:         '75%',
+    fill:         'yellow',
+    isShowStart:  true,
+  });
