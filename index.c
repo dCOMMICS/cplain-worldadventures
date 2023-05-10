ddf7d829adcb8787b40a9930dfefb69ce9bce582
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-10 14:24:53 +0300

    Quiz Intro

---
+++ index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  int ans1, ans2,ans3,ans4,ans5;
+  int point1,point2,point3,point4,point5,point6,point7,point8,point9,point10;
+  int point01,point02,point03,point04,point05,point06,point07,point08,point09,point010;
+  int total1,total2,total3;
+  print("Welcome to The Quiz")
+  return 0;
+}
