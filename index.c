6f037b7acd409ec5b5204e8d361fdb6ec341e178
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-10 14:36:50 +0300

    gAME iNSTRUCTION

--- index.c
+++ index.c
@@ -6,6 +6,28 @@ int main() {
   int point1,point2,point3,point4,point5,point6,point7,point8,point9,point10;
   int point01,point02,point03,point04,point05,point06,point07,point08,point09,point010;
   int total1,total2,total3;
-  printf("Welcome to The Quiz\n\n")
+
+  //GAME INSTRUCTIONS//
+  printf("Welcome to The Quiz\n\n");
+
+  printf("< Press 7 to START\n");
+
+  printf("Press 0 to QUIT \n");
+  
+  //GAME CODE//
+  scanf("%d", &i);
+
+  if (1==7)
+  {
+    printf("the game has STARTED\n\n");
+  }
+  else if(i==0){
+    
+    printf("END");
+  }
+
+  else {
+    printf("Invalid Input");
+  }
   return 0;
 }
