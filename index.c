f06e1c1edc059492d70a7f23ef1bfa30386a413e
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-10 14:26:57 +0300

    fixing

--- index.c
+++ index.c
@@ -6,6 +6,6 @@ int main() {
   int point1,point2,point3,point4,point5,point6,point7,point8,point9,point10;
   int point01,point02,point03,point04,point05,point06,point07,point08,point09,point010;
   int total1,total2,total3;
-  print("Welcome to The Quiz")
+  printf("Welcome to The Quiz\n\n")
   return 0;
 }
