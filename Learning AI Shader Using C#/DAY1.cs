a35ac5047aafd3d2866d97ac663c7aa7bd066006
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-06-23 10:53:44 +0300

    day 1

---
+++ Learning AI Shader Using C#/DAY1.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class KeywordExample : MonoBehaviour
+{
+    public Material material;
+
+    void Start()
+    {
+        CheckShaderKeywordState();
+    }
+
+    void CheckShaderKeywordState()
+    {
+        // Get the instance of the Shader class that the material uses
+        var shader = material.shader;
+
+        // Get all the local keywords that affect the Shader
+        var keywordSpace = shader.keywordSpace;
+
+        // Iterate over the local keywords
+        foreach (var localKeyword in keywordSpace.keywords)
+        {
+            // If the local keyword is overridable (i.e., it was declared with a global scope),
+            // and a global keyword with the same name exists and is enabled,
+            // then Unity uses the global keyword state
+            if (localKeyword.isOverridable && Shader.IsKeywordEnabled(localKeyword.name))
+            {
+                Debug.Log("Local keyword with name of " + localKeyword.name + " is overridden by a global keyword, and is enabled");
+            }
+            // Otherwise, Unity uses the local keyword state
+            else
+            {
+                var state = material.IsKeywordEnabled(localKeyword) ? "enabled" : "disabled";
+                Debug.Log("Local keyword with name of " + localKeyword.name + " is " + state);
+            }            
+        }
+    }
+}
