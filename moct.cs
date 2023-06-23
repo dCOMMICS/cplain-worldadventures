f869084a94e55f17cb67e0eb1f10959bea21710d
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-06-23 10:51:45 +0300

    mo

---
+++ moct.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Created by MOOICT.COM
+// For educational Purposes Only
+namespace Platform_Game_Tutorial_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, jumping, isGameOver;
+        int jumpSpeed;
+        int force;
+        int score = 0;
+        int playerSpeed = 7;
+        int horizontalSpeed = 5;
+        int verticalSpeed = 3;
+        int enemyOneSpeed = 5;
+        int enemyTwoSpeed = 3;
+         public Form1()
+        {
+            InitializeComponent();
+        }
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+            player.Top += jumpSpeed;
+            if (goLeft == true)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight == true)
+            {
+                player.Left += playerSpeed;
+            }
+            // line 52 on source code//
